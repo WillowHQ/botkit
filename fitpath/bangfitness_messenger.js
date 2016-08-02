686d9af06b945db055ed66edf4b06b2e4e553337
Shane Pelletier <lotios611@gmail.com>
2016-08-02 15:44:55 -0300

    Cleaning up code

--- fitpath/bangfitness_messenger.js
+++ fitpath/bangfitness_messenger.js
@@ -27,8 +27,9 @@ controller.setupWebserver(3000, function (err, webserver) {
   });
 });
 
-controller.on('facebook_optin', function (bot, message) {
-  var messageData = {
+function sendGreeting (bot, message) {
+
+  var greetingData = {
     text: "Hi, this is Omnapatopea. I'm a robot!\n\nI'm contacting you because you reached out to Bang Fitness.\n\nI'm not very good with most subjects (because of the whole robot thing) but I can help you get whatever information you need.\n\nAre you interested in:",
     quick_replies: [
       {
@@ -65,168 +66,190 @@ controller.on('facebook_optin', function (bot, message) {
   };
 
   bot.reply(message, messageData);
+}
+
+controller.on('facebook_optin', function (bot, message) {
+  sendGreeting(bot, message);
 });
 
-controller.hears('Visiting BangFitness', 'message_received', function (bot, message) {
-  bot.startConversation(message, function (err, convo) {
-    var messageData = {
-      text: "I can definitely help you with that. Are evenings or daytimes better for you?",
-      quick_replies: [
-        {
-          content_type: "text",
-          title: "Evenings",
-          payload: "EVENINGS"
-        },
-        {
-          content_type: "text",
-          title: "Daytimes",
-          payload: "DAYTIMES"
-        }
-      ]
-    };
-
-    convo.ask(messageData, function (response, convo) {
-      messageData = {
-        text: "Got it. Are you free tomorrow?",
-        quick_replies: [
-          {
-            content_type: "text",
-            title: "Yes",
-            payload: "YES"
-          },
-          {
-            content_type: "text",
-            title: "No",
-            payload: "NO"
-          }
-        ]
-      };
-      convo.ask(messageData, function (response, convo) {
-        if (response.text == 'No') {
-          convo.ask('What\'s another day that would work?', function (response, convo) {
-            convo.ask('What time would be best?', function (response, convo) {
-              convo.say('Ok, I\'ll ask someone from our team to get in contact via e-mail.');
-              convo.next();
-            });
-            convo.next();
-          });
-        } else {
-          convo.ask('What time would be best?', function (response, convo) {
-            convo.say('Ok, I\'ll ask someone from our team to get in contact via e-mail.');
-            convo.next();
-          });
-        }
-        convo.next();
-      });
-      convo.next();
-    });
+function askWhatTimeWouldBeBest (convo) {
+  convo.ask('What time would be best?', function (response, convo) {
+    convo.say('Ok, I\'ll ask someone from our team to get in contact via e-mail.');
     convo.next();
   });
+}
+
+function askForAnotherDay (convo) {
+  convo.ask('What\'s another day that would work?', function (response, convo) {
+    askWhatTimeWouldBeBest();
+    convo.next();
+  });
+}
+
+function askIfFreeTomorrow (convo) {
+  messageData = {
+    text: "Got it. Are you free tomorrow?",
+    quick_replies: [
+      {
+        content_type: "text",
+        title: "Yes",
+        payload: "YES"
+      },
+      {
+        content_type: "text",
+        title: "No",
+        payload: "NO"
+      }
+    ]
+  };
+  convo.ask(messageData, function (response, convo) {
+    if (response.text == 'No') {
+      askForAnotherDay(convo);
+    } else {
+      askWhatTimeWouldBeBest(convo);
+    }
+
+    convo.next();
+  });
+}
+
+function askEveningsOrDaytimes (convo) {
+  var messageData = {
+    text: "I can definitely help you with that. Are evenings or daytimes better for you?",
+    quick_replies: [
+      {
+        content_type: "text",
+        title: "Evenings",
+        payload: "EVENINGS"
+      },
+      {
+        content_type: "text",
+        title: "Daytimes",
+        payload: "DAYTIMES"
+      }
+    ]
+  };
+
+  convo.ask(messageData, function (response, convo) {
+    askIfFreeTomorrow(convo);
+    convo.next();
+  });
+}
+
+function beginVisitingConversation(bot, message) {
+  bot.startConversation(message, function (err, convo) {
+    askEveningsOrDaytimes(convo);
+    convo.next();
+  });
+}
+
+controller.hears('Visiting BangFitness', 'message_received', function (bot, message) {
+  beginVisitingConversation(bot, message);
 });
 
 controller.hears('About our approach', 'message_received', function (bot, message) {
-  bot.startConversation(message, function (err, convo) {
-    messageData = {
-      text: "That would be our pleasure. Would you prefer a phone call or an e-mail?",
-      quick_replies: [
-        {
-          content_type: "text",
-          title: "Phone call",
-          payload: "PHONE_CALL"
-        },
-        {
-          content_type: "text",
-          title: "E-mail",
-          payload: "EMAIL"
-        }
-      ]
-    };
-    convo.ask(messageData, function (response, convo) {
-      if (response.text == 'Phone call') {
-        convo.ask('No problem. What\'s your number?', function (response, convo) {
-          convo.ask('What\'s a good time to call?', function (response, convo) {
-            convo.say('Sounds great. I\'m a robot so I don\'t feel feelings but our team is looking forward to speaking with you!');
-          });
-        });
-      } else {
-        convo.say('Sounds great. I\'m a robot so I don\'t feel feelings but our team is looking forward to speaking with you!');
-      }
-      convo.next();
-    });
-    convo.next();
-  });
+  // bot.startConversation(message, function (err, convo) {
+  //   messageData = {
+  //     text: "That would be our pleasure. Would you prefer a phone call or an e-mail?",
+  //     quick_replies: [
+  //       {
+  //         content_type: "text",
+  //         title: "Phone call",
+  //         payload: "PHONE_CALL"
+  //       },
+  //       {
+  //         content_type: "text",
+  //         title: "E-mail",
+  //         payload: "EMAIL"
+  //       }
+  //     ]
+  //   };
+  //   convo.ask(messageData, function (response, convo) {
+  //     if (response.text == 'Phone call') {
+  //       convo.ask('No problem. What\'s your number?', function (response, convo) {
+  //         convo.ask('What\'s a good time to call?', function (response, convo) {
+  //           convo.say('Sounds great. I\'m a robot so I don\'t feel feelings but our team is looking forward to speaking with you!');
+  //         });
+  //       });
+  //     } else {
+  //       convo.say('Sounds great. I\'m a robot so I don\'t feel feelings but our team is looking forward to speaking with you!');
+  //     }
+  //     convo.next();
+  //   });
+  //   convo.next();
+  // });
 });
 
 controller.hears(['Technique advice', 'Training programs', 'Nutrition'], 'message_received', function (bot, message) {
-  bot.startConversation(message, function (err, convo) {
-    convo.ask('That\'s one of our favourite things. What would you specifically like to learn about?', function (response, convo) {
-      messageData = {
-        text: "That would be our pleasure. Would you prefer a phone call or an e-mail?",
-        quick_replies: [
-          {
-            content_type: "text",
-            title: "Phone call",
-            payload: "PHONE_CALL"
-          },
-          {
-            content_type: "text",
-            title: "E-mail",
-            payload: "EMAIL"
-          }
-        ]
-      };
-      convo.ask(messageData, function (response, convo) {
-        if (response.text == 'Phone call') {
-          convo.ask('No problem. What\'s your number?', function (response, convo) {
-            convo.ask('What\'s a good time to call?', function (response, convo) {
-              convo.say('Sounds great. I\'m a robot so I don\'t feel feelings but our team is looking forward to speaking with you!');
-              convo.next();
-            });
-            convo.next();
-          });
-        } else {
-          convo.say('Sounds great. I\'m a robot so I don\'t feel things but our team is looking forward to speaking with you!');
-        }
-        convo.next();
-      });
-      convo.next();
-    });
-    convo.next();
-  });
+  // bot.startConversation(message, function (err, convo) {
+  //   convo.ask('That\'s one of our favourite things. What would you specifically like to learn about?', function (response, convo) {
+  //     messageData = {
+  //       text: "That would be our pleasure. Would you prefer a phone call or an e-mail?",
+  //       quick_replies: [
+  //         {
+  //           content_type: "text",
+  //           title: "Phone call",
+  //           payload: "PHONE_CALL"
+  //         },
+  //         {
+  //           content_type: "text",
+  //           title: "E-mail",
+  //           payload: "EMAIL"
+  //         }
+  //       ]
+  //     };
+  //     convo.ask(messageData, function (response, convo) {
+  //       if (response.text == 'Phone call') {
+  //         convo.ask('No problem. What\'s your number?', function (response, convo) {
+  //           convo.ask('What\'s a good time to call?', function (response, convo) {
+  //             convo.say('Sounds great. I\'m a robot so I don\'t feel feelings but our team is looking forward to speaking with you!');
+  //             convo.next();
+  //           });
+  //           convo.next();
+  //         });
+  //       } else {
+  //         convo.say('Sounds great. I\'m a robot so I don\'t feel things but our team is looking forward to speaking with you!');
+  //       }
+  //       convo.next();
+  //     });
+  //     convo.next();
+  //   });
+  //   convo.next();
+  // });
 });
 
 controller.hears('Something else', 'message_received', function (bot, message) {
-  bot.startConversation(message, function (err, convo) {
-      messageData = {
-        text: "I\'ll ask on of our team members to get in touch with you. Would you prefer a phone call or an e-mail?",
-        quick_replies: [
-          {
-            content_type: "text",
-            title: "Phone call",
-            payload: "PHONE_CALL"
-          },
-          {
-            content_type: "text",
-            title: "E-mail",
-            payload: "EMAIL"
-          }
-        ]
-      };
-      convo.ask(messageData, function (response, convo) {
-        if (response.text == 'Phone call') {
-          convo.ask('No problem. What\'s your number?', function (response, convo) {
-            convo.ask('What\'s a good time to call?', function (response, convo) {
-              convo.say('Sounds great. I\'m a robot so I don\'t feel feelings but our team is looking forward to speaking with you!');
-              convo.next();
-            });
-            convo.next();
-          });
-        } else {
-          convo.say('Sounds great. I\'m a robot so I don\'t feel things but our team is looking forward to speaking with you!');
-          convo.next();
-        }
-        convo.next();
-      });
-    convo.next();
-  });
+  // bot.startConversation(message, function (err, convo) {
+  //     messageData = {
+  //       text: "I\'ll ask on of our team members to get in touch with you. Would you prefer a phone call or an e-mail?",
+  //       quick_replies: [
+  //         {
+  //           content_type: "text",
+  //           title: "Phone call",
+  //           payload: "PHONE_CALL"
+  //         },
+  //         {
+  //           content_type: "text",
+  //           title: "E-mail",
+  //           payload: "EMAIL"
+  //         }
+  //       ]
+  //     };
+  //     convo.ask(messageData, function (response, convo) {
+  //       if (response.text == 'Phone call') {
+  //         convo.ask('No problem. What\'s your number?', function (response, convo) {
+  //           convo.ask('What\'s a good time to call?', function (response, convo) {
+  //             convo.say('Sounds great. I\'m a robot so I don\'t feel feelings but our team is looking forward to speaking with you!');
+  //             convo.next();
+  //           });
+  //           convo.next();
+  //         });
+  //       } else {
+  //         convo.say('Sounds great. I\'m a robot so I don\'t feel things but our team is looking forward to speaking with you!');
+  //         convo.next();
+  //       }
+  //       convo.next();
+  //     });
+  //   convo.next();
+  // });
 });
