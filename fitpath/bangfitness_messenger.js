29086d099cd84bff522b5266ec184d5959ce48dd
Shane Pelletier <lotios611@gmail.com>
2016-08-02 13:32:37 -0300

    Added some example code

--- fitpath/bangfitness_messenger.js
+++ fitpath/bangfitness_messenger.js
@@ -19,11 +19,115 @@ var controller = Botkit.facebookbot({
 
 var bot = controller.spawn({});
 
-//controller.setupWebserver(3000, function (err, webserver) {
-  controller.createWebhookEndpoints(app, bot, function () {
-    console.log('Bot online');
-    console.log('Free stuff pl0x');
+function create_user_if_new (id, ts) {
+  controller.storage.users.get(id, function (err, user) {
+    if (err) {
+      console.log(err);
+    } else if (!user) {
+      controller.storage.users.save({id: id, created_at: ts});
+    }
   });
+}
+
+function handleFacebook (obj) {
+  controller.debug('GOT A MESSAGE HOOK');
+  if (obj.entry) {
+    for (var e = 0; e < obj.entry.length; e++) {
+      for (var m = 0; m < obj.entry[e].messaging.length; m++) {
+        var facebook_message = obj.entry[e].messaging[m];
+
+        console.log(facebook_message)
+
+        //normal message
+        if (facebook_message.message) {
+
+          var message = {
+              text: facebook_message.message.text,
+              user: facebook_message.sender.id,
+              channel: facebook_message.sender.id,
+              timestamp: facebook_message.timestamp,
+              seq: facebook_message.message.seq,
+              mid: facebook_message.message.mid,
+              attachments: facebook_message.message.attachments,
+          }
+
+          //save if user comes from m.me adress or Facebook search
+          create_user_if_new(facebook_message.sender.id, facebook_message.timestamp)
+
+          controller.receiveMessage(bot, message);
+        }
+        //clicks on a postback action in an attachment
+        else if (facebook_message.postback) {
+
+          // trigger BOTH a facebook_postback event
+          // and a normal message received event.
+          // this allows developers to receive postbacks as part of a conversation.
+          var message = {
+              payload: facebook_message.postback.payload,
+              user: facebook_message.sender.id,
+              channel: facebook_message.sender.id,
+              timestamp: facebook_message.timestamp,
+          };
+
+          controller.trigger('facebook_postback', [bot, message]);
+
+          var message = {
+              text: facebook_message.postback.payload,
+              user: facebook_message.sender.id,
+              channel: facebook_message.sender.id,
+              timestamp: facebook_message.timestamp,
+          };
+
+          controller.receiveMessage(bot, message);
+
+        }
+        //When a user clicks on "Send to Messenger"
+        else if (facebook_message.optin) {
+
+          var message = {
+              optin: facebook_message.optin,
+              user: facebook_message.sender.id,
+              channel: facebook_message.sender.id,
+              timestamp: facebook_message.timestamp,
+          };
+
+          //save if user comes from "Send to Messenger"
+          create_user_if_new(facebook_message.sender.id, facebook_message.timestamp)
+
+          controller.trigger('facebook_optin', [bot, message]);
+        }
+        //message delivered callback
+        else if (facebook_message.delivery) {
+
+          var message = {
+              optin: facebook_message.delivery,
+              user: facebook_message.sender.id,
+              channel: facebook_message.sender.id,
+              timestamp: facebook_message.timestamp,
+          };
+
+          controller.trigger('message_delivered', [bot, message]);
+
+        }
+        else {
+          controller.log('Got an unexpected message from Facebook: ', facebook_message);
+        }
+      }
+    }
+  }
+}
+
+app.post('/webhook', function (req, res) {
+  handleFacebook(req.body);
+
+  res.send('Ok');
+});
+
+//controller.setupWebserver(3000, function (err, webserver) {
+  // controller.createWebhookEndpoints(app, bot, function () {
+  //   console.log('Bot online');
+  //   console.log('Free stuff pl0x');
+  // });
 //});
 
 controller.on('facebook_optin', function (bot, message) {
